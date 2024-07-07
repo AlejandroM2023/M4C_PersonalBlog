c89301cb67920e2a57ff845c396067d5d10c81a9
AlejandroM2023 <alejandromeza437@gmail.com>
2024-07-07 00:36:44 -0500

    form validation and logic to add to local storage

--- assets/js/form.js
+++ assets/js/form.js
@@ -7,16 +7,46 @@ const submitButton = document.getElementById("submit");
 //have all text boxes as array variable to minize variable/clean code
 const inputBoxes = document.querySelectorAll("input");
 
+//message area
+const messageArea = document.getElementById("message");
+
+
 
 //funtions
 
+//to earase error messages
+function resetErrors(){
+   messageArea.innerHTML = "";
+}
+
+//check if boxes are empty if not clear previous error message and present new errors and return false if true return true
+function boxIsNotEmpty(){
+    let empty = false;
+    let emptyCount = 0;
+    resetErrors();
+    for(let i =0; i<inputBoxes.length;i++){
+        if(inputBoxes[i].value == ""){
+            const lineBreak = document.createElement("br")
+            messageArea.append(`${inputBoxes[i].id} is empty`);
+            messageArea.appendChild(lineBreak);
+            emptyCount ++;
+        }
+    }
+    if(emptyCount == 0){
+        empty = true;
+    }
+    return empty;
+}
 
 
 
 //button calls 
 
-//event in function to prevent call since button inside form
+//event in function to prevent call since button inside form, checks if boxes are empty if not adds a post to a local storage variable
 submitButton.addEventListener('click',function(event){
     event.preventDefault();
-    console.log(inputBoxes[1].value);
+    if(boxIsNotEmpty()){
+        addPost(inputBoxes[0].value,inputBoxes[1].value,inputBoxes[2].value);
+    }
+
 });
---
+++ assets/js/logic.js
@@ -0,0 +1,15 @@
+//check of our array of object already exist if not create it
+if(!localStorage.getItem("postObjectArray")){
+    const obj = [];
+    localStorage.setItem("postObjectArray",JSON.stringify(obj));
+}
+
+
+function addPost(userName,title,txt){
+
+    const post = {UserName: userName,Title: title, content: txt};
+    const postArr = JSON.parse(localStorage.getItem("postObjectArray")) ;
+    postArr.push(post);
+    localStorage.setItem("postObjectArray",JSON.stringify(postArr));
+
+}
